07de5e8b247d4dc3d5261878974ae4c956e219a5
unknown <3702740@qq.com>
2019-11-24 17:16:31 +0800

    更新1

--- script/packajax.js
+++ script/packajax.js
@@ -456,6 +456,10 @@ var _getHttpsData = function(actionUrl, param, funRecall, errFunRecall) {
 			var msg = err.msg + '(' + err.code + ')'
 			console.log (title)
 			console.log (msg)
+			// api.alert({
+			// 	title : title,
+			// 	msg : msg
+			// });
 		}
 	});
 
